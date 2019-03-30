cf3ac8c572149d0dddc3c67262e3de6c4e382c80
liaohq <237776777@qq.com>
2019-03-30 15:47:29 +0800

    redis

---
+++ Common/Sql/RedisConn.cpp
@@ -0,0 +1,93 @@
+#include "RedisConn.h"
+#include "stdafx.h"
+
+
+RedisConn  g_RedisConn;
+
+
+RedisConn::RedisConn()
+{
+
+}
+
+RedisConn::~RedisConn()
+{
+
+}
+
+void	RedisConn::PreInit()
+{
+	m_connHandle = redisConnect((char*)"127.0.0.1", 6379);
+	if (m_connHandle->err) 
+	{
+		GAME_ASSERT_MSG(FALSE,"Redis Connect error:%s\n",m_connHandle->errstr);
+		return;
+	}
+
+	PRINT_MSG("Redis Connect Success");
+}
+
+void	RedisConn::Init()
+{
+
+}
+
+
+void	RedisConn::Start()
+{
+	Test();
+}
+
+
+void	RedisConn::Stop()
+{
+
+}
+
+void	RedisConn::CleanUp()
+{
+
+}
+
+void	RedisConn::Test()
+{
+
+	//set data
+	struct RedisData
+	{
+		int English;
+		int Math;
+	};
+
+	RedisData* tmp_data=new RedisData;
+	tmp_data->English=100;
+	tmp_data->Math=200;
+
+	
+	//INT64  key=(INT64)tmp_data;
+	//PRINT_MSG(key);
+
+	
+	size_t data_len=sizeof(tmp_data);
+	const char* send_msg=(const char*)tmp_data;
+	
+	m_reply = (redisReply*)redisCommand(m_connHandle,"set i %b", send_msg, data_len);
+	if(m_reply == NULL)
+	{
+		GAME_ASSERT_MSG(FALSE,"Redis Reply fail,err_str:%s\n",m_connHandle->errstr);
+		return;
+	}
+
+	printf("Redis Replay:%s\n",m_reply->str);
+
+	m_reply = (redisReply*)redisCommand(m_connHandle,"get i");
+	if(m_reply == NULL)
+	{
+		GAME_ASSERT_MSG(FALSE,"Redis Reply fail,err_str:%s\n",m_connHandle->errstr);
+		return;
+	}
+
+	RedisData* msg = (RedisData*)m_reply->str;
+	printf("Redis Replay,len:%d, :%d %d \n", m_reply->len, msg->English, msg->Math );
+
+}
---
+++ Common/Sql/RedisConn.h
@@ -0,0 +1,40 @@
+﻿/*
+ * author: lhq
+ * date  : 2019.3.23
+ * bref	 : redis connect base interface
+ */
+
+#ifndef	_REDIS_CONN_H
+#define	_REDIS_CONN_H
+
+#include "Common.h"
+#include "Base/Module.h"
+#include "SystemApi.h"
+
+class RedisConn:public Module
+{
+public:
+	RedisConn();
+	~RedisConn();
+
+public:
+	void				PreInit();
+	void				Init();
+	void				Start();
+	void				Stop();
+	void				CleanUp();
+
+public:
+	void				Test();
+private:
+	redisContext*		m_connHandle;
+	redisReply*			m_reply;
+};
+
+extern   RedisConn  g_RedisConn;
+
+#ifndef  REDIS_MGR
+#define  REDIS_MGR   g_RedisConn
+#endif
+
+#endif
